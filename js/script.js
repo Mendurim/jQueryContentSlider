7699e646089bb12530f6097a9c48653f26f555ad
mendurim <mendurim.r@gmail.com>
2016-03-07 13:31:16 +0100

    Added jQuery for next arrow functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-	alert('Doc is loaded'); 
+	//Set options 
+	var speed = 500; // fade speed
+	var autoSwitch = true; // auto slider options
+	var autoSwitchSpeed = 4000; // auto slider speed
+
+	// Add initial active class
+	$('.slide').first().addClass('active'); 
+
+
+	//Hide all slides
+	$('.slide').hide(); 
+
+	//Show first slide
+	$('.active').show(); 
+	
+	$('#next').on('click', function(){
+		$('.active').removeClass('active').addClass('oldActive'); 
+		if($('oldActive').is(':last-child')){
+			$('.slide').first().addClass('active'); 
+		} else {
+			$('.oldActive').next().addClass('active'); 
+		}
+		$('oldActive').removeClass('oldActive'); 
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed); 
+	}); 
 }); 
